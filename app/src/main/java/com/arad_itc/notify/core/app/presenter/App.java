67d8952dbd39fc4079f7a1286276007522540cda
akbari <politecoder97@gmail.com>
2022-05-21 11:41:01 +0430

    01-02-31

---
+++ app/src/main/java/com/arad_itc/notify/core/app/presenter/App.java
@@ -0,0 +1,66 @@
+package com.arad_itc.notify.core.app.presenter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.arad_itc.notify.core.amq.domain.entities.AMQMessage;
+import com.arad_itc.notify.core.amq.domain.entities.ConnectionStatus;
+import com.arad_itc.notify.core.amq.domain.repositories.OnAradBrokerListener;
+import com.arad_itc.notify.core.amq.presentation.AradBroker;
+import com.arad_itc.notify.core.app.presenter.amq.presentation.manager.RXBus;
+import com.arad_itc.notify.core.app.presenter.amq.presentation.manager.RxBusMessageEvent;
+import com.arad_itc.notify.core.notification.AradPushNotificationManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import androidx.annotation.NonNull;
+
+public class App extends Application {
+
+  public static AradBroker connection;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    FirebaseApp.initializeApp(this);
+    createConnection();
+  }
+
+  void createConnection() {
+    FirebaseMessaging.getInstance().getToken()
+      .addOnCompleteListener(new OnCompleteListener<String>() {
+        @Override
+        public void onComplete(@NonNull Task<String> task) {
+          if (!task.isSuccessful()) {
+            Log.w("TAG", "FCM registration token failed", task.getException());
+            return;
+          }
+          // Get new FCM registration token
+          String token = task.getResult();
+          Log.i("AradBroker", "onComplete, token: " + token);
+          connection = AradBroker.getInstance(getApplicationContext())
+            .setAppId("626d0d549c4704dcf2258b46")
+            .setCredential("989135646251", "u68p@K$$Q@P8$Z$y$1G7g4q1@j@fuh1T")
+            .setUseTls(false)
+            .setHost("192.168.100.55", 1883)
+            .setPreferredKey("9IDiF8moM8oNhxZE+5D94g==")
+            .setDeviceToken(token)
+            .setOnBrokerListener(new OnAradBrokerListener() {
+              @Override
+              public void onConnectionStatus(ConnectionStatus status) {
+                RXBus.publish(new RxBusMessageEvent.OnConnectionStatusChanged(status));
+              }
+
+              @Override
+              public void onMessageArrived(AMQMessage message) {
+                RXBus.publish(new RxBusMessageEvent.OnMessageArrived(message));
+              }
+            })
+            .connect();
+        }
+      });
+  }
+}
--- app/src/main/java/com/arad_itc/notify/core/app/presenter/MainActivity.java
+++ app/src/main/java/com/arad_itc/notify/core/app/presenter/MainActivity.java
@@ -3,6 +3,7 @@ package com.arad_itc.notify.core.app.presenter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.arad_itc.notify.core.amq.domain.entities.AMQMessage;
 import com.arad_itc.notify.core.amq.domain.entities.ConnectionStatus;
@@ -10,36 +11,33 @@ import com.arad_itc.notify.core.amq.domain.repositories.OnAradBrokerListener;
 import com.arad_itc.notify.core.amq.presentation.AradBroker;
 import com.arad_itc.notify.core.app.presenter.amq.presentation.manager.RXBus;
 import com.arad_itc.notify.core.app.presenter.amq.presentation.manager.RxBusMessageEvent;
+import com.arad_itc.notify.core.notification.AradPushNotificationManager;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    setupConnectionListener();
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-
-//    AradBroker.getInstance(this)
-//      .setAppId("626d0d549c4704dcf2258b46")
-//      .setCredential("989135646251", "i1@bBW?$9v$??16$2Gg8BJ9$jr?z956Q")
-//      .setUseTls(false)
-//      .setHost("192.168.100.55", 1883)
-//      .setPreferredKey("AMplKJBBBLnskVfJEcW8Ww==")
-//
-//      .setOnBrokerListener(new OnAradBrokerListener() {
-//        @Override
-//        public void onConnectionStatus(ConnectionStatus connectionStatus) {
-//          RXBus.publish(new RxBusMessageEvent.OnConnectionStatusChanged(connectionStatus));
-//        }
-//
-//        @Override
-//        public void onMessageArrived(AMQMessage amqMessage) {
-//          RXBus.publish(new RxBusMessageEvent.OnMessageArrived(amqMessage));
-//        }
-//      })
-//      .connect();
-
     setContentView(R.layout.activity_main);
 
 
+  }
+
+  private void setupConnectionListener() {
+    RXBus.listen(RxBusMessageEvent.OnConnectionStatusChanged.class).subscribe(
+      o -> {
+        RxBusMessageEvent.OnConnectionStatusChanged messageEvent = (RxBusMessageEvent.OnConnectionStatusChanged) o;
+        getSupportActionBar().setTitle(messageEvent.getConnectionStatus().name());
+      }
+    );
   }
 }
